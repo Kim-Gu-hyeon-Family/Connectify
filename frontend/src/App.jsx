58f9fc350dc1fedf4d0e6cb5e76c60d4bc8a5d18
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-25 08:47:59 +0900

    d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Signup from './pages/Auth/Signup/Signup';
-import Signin from './pages/Auth/Signin/Signin';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom";
+import Signup from "./pages/Auth/Signup/Signup";
+import Signin from "./pages/Auth/Signin/Signin";
 
 const App = () => (
     <Router>
         <Routes>
-            <Route path="/" element={<Navigate to="/signup" />} />
+            <Route path="/" element={<Navigate to="/signin" />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/signin" element={<Signin />} />
         </Routes>
