02dc65f2096cfe1e81f8a3342cf3c40cc910ec0c
kgh852 <gimguhyun0502@gmail.com>
2024-12-09 20:06:15 +0900

    feat: commit

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const postRoutes = require('./routes/postRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const { initializeSocket } = require('./services/socketService');
+const chatRoutes = require('./routes/chatRoutes');
+
+const http = require('http')
+const cors = require('cors')
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/user', authRoutes);
+app.use('/posts', postRoutes); 
+app.use('/api/chat', chatRoutes);
+
+const server = http.createServer(app);
+initializeSocket(server);
+
+app.use(cors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }));
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
